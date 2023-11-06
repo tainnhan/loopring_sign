57934c59a30b685e61b1966e1b24fc4f76cdca1f
tnguyenn <tnguyenn@hs-mittweida.de>
2023-11-06 12:56:32 +0100

    feat: sign function

--- src/poseidon/eddsa.rs
+++ src/poseidon/eddsa.rs
@@ -27,14 +27,16 @@ For Hash-EdDSA, the message `M` is compressed before H(R,A,M)
 For further information see: https://ed2519.cr.yp.to/eddsa-20150704.pdf
 */
 
+use std::fmt::format;
+
 use super::{
     field::{FQ, SNARK_SCALAR_FIELD},
-    jubjub::{Point, JUBJUB_L},
+    jubjub::{Point, JUBJUB_E, JUBJUB_L},
     permutation::{self, Poseidon},
 };
 use crate::util::helpers::{generate_signature_base_string, sha256_snark, to_bytes_32};
 use num_bigint::{BigInt, Sign};
-use num_traits::{Num, Zero};
+use num_traits::{sign, Num, Zero};
 use sha2::{Digest, Sha512};
 
 pub struct Signature {
@@ -47,11 +49,32 @@ impl Signature {
         Signature { R, s }
     }
 
-    pub fn to_string(self) -> String {
+    pub fn to_string(&self) -> String {
         format!("{} {} {}", &self.R.x().n(), &self.R.y().n(), &self.s.n())
     }
 }
 
+pub struct SignedMessage {
+    A: Point,
+    sig: Signature,
+    msg: BigInt,
+}
+
+impl SignedMessage {
+    pub fn new(A: Point, sig: Signature, msg: BigInt) -> Self {
+        SignedMessage { A, sig, msg }
+    }
+
+    pub fn to_string(&self) -> String {
+        format!(
+            "{} {} {} {}",
+            self.A.x().n(),
+            self.A.y().n(),
+            self.sig.to_string(),
+            self.msg
+        )
+    }
+}
 pub struct SignatureScheme;
 
 impl SignatureScheme {
@@ -60,17 +83,25 @@ impl SignatureScheme {
         Point::generate()
     }
 
-    pub fn sign(private_key: BigInt, hash: BigInt) {
+    pub fn sign(private_key: BigInt, hash: BigInt) -> SignedMessage {
         let base_point = Self::base_point();
 
-        let A = &base_point * &private_key;
-        let M = hash; // prehash message
-        let r = Self::hash_secret(FQ::new(private_key), &M);
+        let A = &base_point * &private_key; // A = k * P -> Public key
+
+        let M = hash.clone(); // prehash message
+        let r = Self::hash_secret(FQ::new(private_key.clone()), &M);
 
         let R = &base_point * &r;
 
-        Self::hash_public(&R, &A, M);
-        ()
+        let t = Self::hash_public(&R, &A, M);
+        let S = (r + (private_key * t)) % JUBJUB_E.clone();
+
+        let signature_result = Signature::new(R, FQ::new(S));
+
+        let signed_message = SignedMessage::new(A, signature_result, hash);
+
+        //let signature_result = Sig
+        signed_message
     }
 
     /*
@@ -135,13 +166,14 @@ pub fn generate_eddsa_signature(
             Err(_) => BigInt::zero(),
         };
 
-    SignatureScheme::sign(private_key_big_int, hash)
+    SignatureScheme::sign(private_key_big_int, hash);
+    ()
 }
 
 #[cfg(test)]
 mod tests {
     use num_traits::One;
-    use std::str::FromStr;
+    use std::{str::FromStr, time::Instant};
 
     use super::*;
     #[test]
@@ -208,5 +240,20 @@ mod tests {
             )
             .unwrap()
         );
+    }
+
+    #[test]
+    fn sign_test() {
+        let msg_hash = BigInt::from_str(
+            "20693456676802104653139582814194312788878632719314804297029697306071204881418",
+        )
+        .unwrap();
+        let private_key = BigInt::from(1);
+        let start = Instant::now();
+
+        let signed = SignatureScheme::sign(private_key, msg_hash);
+        let duration = start.elapsed();
+        println!("{}", duration.as_secs());
+        assert_eq!(signed.to_string(), "16540640123574156134436876038791482806971768689494387082833631921987005038935 20819045374670962167435360035096875258406992893633759881276124905556507972311 4991609103248925747358645194965349262579784734809679007552644294476920671344 423391641476660815714427268720766993055332927752794962916609674122318189741 4678160339597842896640121413028167917237396460457527040724180632868306529961 20693456676802104653139582814194312788878632719314804297029697306071204881418" )
     }
 }
